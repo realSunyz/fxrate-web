8607c8c6cbdf8d5743c9e962f68157096890ac72
Seven SUN <i@sunyz.net>
2025-02-12 17:56:24 +0800

    feat: add site title and description

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Icons } from "@/components/icons"
 // import { ModeSwitcher } from "@/components/mode-switcher"
 
-export default function NavBar() {
+export function NavBar() {
     return (
         <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="mx-auto max-w-7xl px-4">
--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,9 @@
+export const siteConfig = {
+    title: "fxRate Web",
+    description: "fxRate Web Next Generation",
+    url: "https://fxrate.sunyz.dev",
+};
+  
 export const META_THEME_COLORS = {
     light: "#ffffff",
     dark: "#09090b",
