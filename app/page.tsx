e446a46449dcd92392e8537f103e35b6ddd83f1b
Seven SUN <i@sunyz.net>
2025-02-12 17:59:11 +0800

    fix: change the location of dark mode switcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { ModeSwitcher } from "@/components/mode-switcher";
 
 export default function Home() {
   return (
     <>
+      <div className="fixed bottom-4 right-4 z-50">
+        <ModeSwitcher />
+      </div>
     </>
   );
 }
