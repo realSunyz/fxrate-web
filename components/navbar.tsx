cb23333179486ccfcb9820931eb05a4ef9e6c27c
Seven SUN <i@sunyz.net>
2025-02-12 17:48:42 +0800

    feat: add logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,15 @@
 "use client"
 
 import * as React from "react"
-import { ModeSwitcher } from "@/components/mode-switcher"
+import { Icons } from "@/components/icons"
+// import { ModeSwitcher } from "@/components/mode-switcher"
 
 export default function NavBar() {
     return (
         <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="mx-auto max-w-7xl px-4">
-                <div className="flex h-14 items-center justify-between">
-                    fxRate
-                    <ModeSwitcher />
+                <div className="flex h-14 items-center justify-center">
+                    <Icons.logo className="h-7" />
                 </div>
             </div>
         </header>
